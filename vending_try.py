dbc86cb02b9c6fe3fe95dc6ae5195494a57e7930
Sulayman-AlWafie <sully.alwafie@gmail.com>
2024-06-15 10:34:59 -0500

    Create vending_try.py

---
+++ vending_try.py
@@ -0,0 +1,17 @@
+def main():
+    while True:
+        amount_due = 50
+        coin = int(input(" Please enter 25,10,5,1:"))
+        amount_due = amount_due - coin
+        print(f"Amount Due{amount_due}")
+
+        
+       
+
+
+
+
+
+
+
+main()
