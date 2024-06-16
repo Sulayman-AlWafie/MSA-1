12fb553799167cdce5c5521f085e5970a8401396
Sulayman-AlWafie <sully.alwafie@gmail.com>
2024-06-16 11:59:07 -0500

    Update vending_try.py

--- vending_try.py
+++ vending_try.py
@@ -1,16 +1,20 @@
 def main():
     amount_due = 50
-    print(f"Amount Due: {amount_due}")
+    
     while True:
-        coin = int(input("Please enter 25,10,5,1:"))
-        amount_due = amount_due - coin 
         print(f"Amount Due {amount_due}")
+        coin =  input("Please enter 25,10,5,1:")
+        if (coin.isnumeric()):
+            coin = int(coin)
+            if((coin) == 1 or (coin) == 5 or (coin) == 10 or (coin) == 25):
+                amount_due = amount_due - coin
+            else:
+               print("Error: Please enter a 1, 5, 10, or 25")  
+        else:
+            print("Error: Please enter a 1, 5, 10, or 25")
         if amount_due < 0:
-            print(f"Return Amount{amount_due}")
-        
-        
-
-       
+            print(f"Return Amount {amount_due * -1} gyatt")
+            break
 
 
 
