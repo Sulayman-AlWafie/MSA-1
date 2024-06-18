f55c3653676e24b1c051670581a61a550502bae2
Sulayman-AlWafie <sully.alwafie@gmail.com>
2024-06-18 11:26:42 -0500

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,7 @@
+#create a program that will be able to decide wether or not the input given is valid or not
+#If input is valid then the input should then be used to calculate what the user wants to calculate
+#If not ignore the input anf reprompt
+
+def main():
+
+    
